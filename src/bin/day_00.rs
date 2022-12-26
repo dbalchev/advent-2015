62affa17f42ba35c0b0e070d907c8aac00036d62
Daniel Balchev <dbalchev@gmail.com>
2022-12-26 12:56:08 +0200

    Add tests

--- src/bin/day_00.rs
+++ src/bin/day_00.rs
@@ -2,6 +2,7 @@ use std::io::BufRead;
 
 use aoc_template_rs::aoc::{solution::Solution, utils::AnyResult};
 
+#[derive(PartialEq, Eq, Debug)]
 struct Day00 {
     lines: Vec<String>,
 }
@@ -26,4 +27,46 @@ impl Solution for Day00 {
 
 fn main() -> AnyResult<()> {
     Day00::day_main()
+}
+
+#[cfg(test)]
+mod day00_tests {
+    use aoc_template_rs::aoc::solution::Solution;
+
+    use super::Day00;
+    fn example_data() -> Day00 {
+        Day00 {
+            lines: vec![
+                "1000".to_string(),
+                "2000".to_string(),
+                "3000".to_string(),
+                "".to_string(),
+                "4000".to_string(),
+                "".to_string(),
+                "5000".to_string(),
+                "6000".to_string(),
+                "".to_string(),
+                "7000".to_string(),
+                "8000".to_string(),
+                "9000".to_string(),
+                "".to_string(),
+                "10000".to_string(),
+            ],
+        }
+    }
+    #[test]
+    fn test_parsing() {
+        assert_eq!(
+            example_data(),
+            Day00::parse_input(Day00::example_data_reader().unwrap()).unwrap()
+        );
+    }
+    #[test]
+    fn test_part_1() {
+        assert_eq!(14, example_data().solve_part_1().unwrap());
+    }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(41, example_data().solve_part_2().unwrap());
+    }
 }
